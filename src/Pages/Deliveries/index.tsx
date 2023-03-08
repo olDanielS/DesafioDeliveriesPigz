fc933c6e79aabab29fdc8c8afed77fc1bfa5fd90
olDanielS <danielnrt80@gmail.com>
2023-03-08 13:05:10 -0300

    Finaliznado tela de nova entrega

--- src/Pages/Deliveries/index.tsx
+++ src/Pages/Deliveries/index.tsx
@@ -1,5 +1,8 @@
-import { Container, Header, InfoDeliverie, TitleInfoDeliverie, ValueInfoDeliverie,
-    CardView,CardInformations, CardFirstTitle,CardSecondTitle
+import {
+    Container, Header, InfoDeliverie, TitleInfoDeliverie, ValueInfoDeliverie,
+    CardView, CardInformations, CardFirstTitle, CardSecondTitle, ContentView, OrangePointer,
+    WhitePointer, DeliveriesTitle, DeliveriesInfo, Content,BtnFooter,TextBtnFooter,
+    Footer
 } from './styles'
 
 import FontAwesome from '@expo/vector-icons/FontAwesome';
@@ -20,16 +23,45 @@ export default function Deliveries() {
 
             <InfoDeliverie>
                 <TitleInfoDeliverie>Valor da Entrega</TitleInfoDeliverie>
-                <ValueInfoDeliverie style={{fontSize: 35}}>R$ 13,75</ValueInfoDeliverie>
+                <ValueInfoDeliverie style={{ fontSize: 35 }}>R$ 13,75</ValueInfoDeliverie>
             </InfoDeliverie>
 
             <CardView>
-                <FontAwesome name="motorcycle" size={24} color={"#FFF"}/>
+                <FontAwesome name="motorcycle" size={24} color={"#FFF"} />
                 <CardInformations>
                     <CardFirstTitle>Entrega</CardFirstTitle>
                     <CardSecondTitle>Percurso Total: 8km</CardSecondTitle>
                 </CardInformations>
             </CardView>
+
+            <ContentView>
+                <Content>
+                    <DeliveriesTitle>Coleta</DeliveriesTitle>
+                </Content>
+                <Content>
+                    <DeliveriesInfo>Restaurante Recanto da Peixada</DeliveriesInfo>
+                    <DeliveriesInfo>Distancia: 2km</DeliveriesInfo>
+                </Content>
+                <Content>
+                    <DeliveriesTitle>Entrega</DeliveriesTitle>
+                </Content>
+                <Content>
+                    <DeliveriesInfo>Av: Cabo dos Soldados - Caranã</DeliveriesInfo>
+                    <DeliveriesInfo>Distancia: 6km</DeliveriesInfo>
+                </Content>
+
+            </ContentView>
+            <Footer>
+                <BtnFooter bg={"accept"}>
+                    <FontAwesome name="check" size={24} color={"#FFF"} />
+                    <TextBtnFooter tx={"accept"}>Aceitar</TextBtnFooter>
+                </BtnFooter>
+
+                <BtnFooter bg={"refuse"} style={{borderWidth: 1}}>
+                <FontAwesome name="close" size={24} color={"#E8453E"} />
+                    <TextBtnFooter tx={"refuse"}>Rejeitar</TextBtnFooter>
+                </BtnFooter>
+            </Footer>
         </Container>
     )
 }
--- src/Pages/Deliveries/styles.ts
+++ src/Pages/Deliveries/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-import { OrangePrimaryColor, BlackPrimaryColor, BlackSecondaryColor, WhitePrimaryColor} from '../globalStyles';
+import { OrangePrimaryColor, BlackPrimaryColor, BlackSecondaryColor, WhitePrimaryColor } from '../globalStyles';
 
 export const Container = styled.View`
     flex: 1;
@@ -9,7 +9,7 @@ export const Container = styled.View`
 `
 export const Header = styled.View`
     flex-direction: row;
-    margin-top: 45px;
+    margin-top: 25px;
     margin-bottom: 10px;
     align-items: center;
     justify-content: space-around;
@@ -36,6 +36,7 @@ export const CardView = styled.View`
     padding: 10px;
     flex-direction: row;
     align-items: center;
+    margin-bottom: 15px;
 `
 export const CardInformations = styled.View`
     padding-left: 10px;
@@ -47,4 +48,59 @@ export const CardFirstTitle = styled.Text`
 export const CardSecondTitle = styled.Text`
     font-size: 12px;
     color: ${WhitePrimaryColor};
+`
+export const ContentView = styled.View`
+    flex-direction: column;
+    justify-content: center;
+    padding: 15px;
+    margin-left: 15px;
+    
+`
+export const OrangePointer = styled.View`   
+    height: 15px;
+    width: 15px;
+    background-color: ${OrangePrimaryColor};
+    border-radius: 8px;
+    margin-right: 15px;
+`
+export const WhitePointer = styled.View`
+    height: 9px;
+    width: 9px;
+    border-color: ${OrangePrimaryColor};
+    background-color: ${WhitePrimaryColor};
+    border-radius: 8px;
+    border-width: 1px;
+    margin-right: 15px;
+`
+export const Content = styled.View`
+   flex-direction: column;
+   margin-bottom: 10px;
+   
+`
+export const DeliveriesTitle = styled.Text`
+   font-size: 18px;
+   font-weight: 600;
+   color: ${OrangePrimaryColor};
+`
+export const DeliveriesInfo = styled.Text`
+   font-size: 14px;
+   color: #707070;
+`
+export const Footer = styled.View`
+   flex-direction: column;
+`
+export const BtnFooter = styled.TouchableOpacity`
+    background-color: ${props => props.bg == "accept" ? OrangePrimaryColor : WhitePrimaryColor};
+    border-color: ${props => props.bg == "refuse" && '#E8453E'};
+    border-radius: 16px;
+    padding: 10px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 15px;
+`
+export const TextBtnFooter = styled.Text`
+   font-size: 18px;
+    color: ${props => props.tx == "accept" ? WhitePrimaryColor : '#E8453E'};
+    padding-left: 10px;
 `
